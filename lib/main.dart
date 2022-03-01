727bca71acd7a6c6fc1bb282e3061b6a6ec75f9e
Sebastian Rendon Arteaga <serendona@unal.edu.co>
2022-03-01 15:27:56 -0500

    Config prooject in my PC

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_components/router/app_routes.dart';
 
-import 'screens/screens.dart';
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
